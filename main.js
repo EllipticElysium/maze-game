fefa460535fdfef445343ea0fd6eb05f292c0bbe
ElipticNight <secondary@aidan-byrne.org.uk>
2021-02-08 14:42:35 +0000

    some rooms are randomly deleted during maze creation

--- main.js
+++ main.js
@@ -1,15 +1,60 @@
 const settings = {
-    gridSize: 10
+    gridSize: 4
 };
 
 let gameFinished = false;
 
 let rooms = [];
-let currentRoom = Math.floor(Math.random() * (settings.gridSize ** 2));
+let currentRoom = null;
 
 function setupGame() {
+    rooms = [];
     for (i = 0; i < settings.gridSize ** 2; i++) {
         rooms.push(new room(i, settings.gridSize));
+    }
+
+    for (i = 0; i < rooms.length; i++) {
+        if(Math.random() <= 0.2) {
+            deleteRoom(i);
+        }
+    }
+
+    currentRoom = Math.floor(Math.random() * (settings.gridSize ** 2));
+}
+
+function deleteRoom(id) {
+    let up = rooms[id].up
+    if(up !== null) {
+        rooms[up].bottom = null
+    }    
+    let right = rooms[id].right
+    if(right !== null) {
+        rooms[right].left = null
+    }    
+    let down = rooms[id].down
+    if(down !== null) {down
+        rooms[down].up = null
+    }    
+    let left = rooms[id].left
+    if(left !== null) {
+        rooms[left].right = null
+    }
+    rooms[id] = null;
+}
+
+
+function displayMap() {
+    let map = [];
+    for (i = 0; i < settings.gridSize; i++) {
+        map.push([]);
+        for (j = 0; j < settings.gridSize; j++) {
+            if(rooms[settings.gridSize * i + j] !== null) {
+                map[i].push(1);
+            } else {
+                map[i].push(0);
+            }
+        }
+        console.log(map[i]);
     }
 }
 
@@ -35,6 +80,7 @@ function nextTurn(action) {
 
 function playGame() {
     setupGame();
+    displayMap();
     displayRoom(currentRoom);
 }
 
