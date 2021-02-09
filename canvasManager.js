6290ee973c4dd79986aadf4e503e678bab4ab521
ElipticNight <secondary@aidan-byrne.org.uk>
2021-02-09 10:45:46 +0000

    added permenant walls

--- canvasManager.js
+++ canvasManager.js
@@ -9,10 +9,22 @@ const ctxPlayer = canvasPlayer.getContext('2d');
 
 ctxBackground.beginPath();
 ctxBackground.moveTo(0, 0);
+ctxBackground.lineTo(canvasBackground.width*(5/12), 0);
+ctxBackground.moveTo(canvasBackground.width*(7/12), 0);
 ctxBackground.lineTo(canvasBackground.width, 0);
+
+ctxBackground.lineTo(canvasBackground.width, canvasBackground.height*(5/12));
+ctxBackground.moveTo(canvasBackground.width, canvasBackground.height*(7/12));
 ctxBackground.lineTo(canvasBackground.width, canvasBackground.height);
+
+ctxBackground.lineTo(canvasBackground.width*(7/12), canvasBackground.height);
+ctxBackground.moveTo(canvasBackground.width*(5/12), canvasBackground.height);
 ctxBackground.lineTo(0, canvasBackground.height);
+
+ctxBackground.lineTo(0, canvasBackground.height*(7/12));
+ctxBackground.moveTo(0, canvasBackground.height*(5/12));
 ctxBackground.lineTo(0, 0);
+
 ctxBackground.stroke();
 
 
