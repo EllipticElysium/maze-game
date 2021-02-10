78f52b587a88f29d42155e66d372c4246185820e
ElipticNight <secondary@aidan-byrne.org.uk>
2021-02-10 17:08:44 +0000

    fixed bug causing player to collect all treasure in room

--- roomItems.js
+++ roomItems.js
@@ -36,17 +36,19 @@ class Item {
 
 class Treasure extends Item {
     value = 0;
+    used = false;
 
     constructor(index) {
         super(canvasBackground.height/20);
         this.index = index;
-        this.value = Math.floor(Math.random() * (10)) * 100;
+        this.value = Math.floor(Math.random() * (9)) * 100 + 100;
     }
 
     detectHit() {
-        if(this.hit(player)) {
+        if(this.hit(player) && this.used === false) {
+            this.used = true;
             player.updateScore(this.value);
-            rooms[currentRoom].contents.treasure.pop();
+            rooms[currentRoom].contents.treasure.splice(this.index, 1);
         }
     }
 }
